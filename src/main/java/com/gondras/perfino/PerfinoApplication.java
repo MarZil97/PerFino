e1c3770c2142e9840455341580100a05a7cec030
Martynas zilius <ziliusmartynas10@gmail.com>
2024-09-18 14:41:21 +0300

    MZI-001: Version Control::Set Up Git Repository

---
+++ src/main/java/com/gondras/perfino/PerfinoApplication.java
@@ -0,0 +1,13 @@
+package com.gondras.perfino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gondras/perfino/PerfinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gondras.perfino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
